3e916c5a92d722e67c299612a904a787f4a43c77
Justin McCormick <me@justinmccormick.com>
2022-03-27 15:38:50 -0400

    fix: do not specify refs for checkout step

--- packages/@pdkit/github/github/steps/GithubCheckoutStep.ts
+++ packages/@pdkit/github/github/steps/GithubCheckoutStep.ts
@@ -6,10 +6,6 @@ export class GithubCheckoutStep extends GithubJobStep {
     super(scope, id, {
       name: "Checkout",
       uses: "actions/checkout@v2",
-      with: {
-        ref: "${{ github.event.pull_request.head.ref }}",
-        repository: "${{ github.event.pull_request.head.repo.full_name }}",
-      },
       ...props,
     });
   }
