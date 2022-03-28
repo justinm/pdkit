481d0ca71876b463eeec647bdd2cc36ee1c06ec4
Justin McCormick <me@justinmccormick.com>
2022-03-28 13:42:42 -0400

    fix: cli issues

--- packages/@pdkit/cli/index.ts
+++ packages/@pdkit/cli/index.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env ts-node
 
-import path from "path";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require("path");
 
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 require(path.join(__dirname, "./pdkit"));
