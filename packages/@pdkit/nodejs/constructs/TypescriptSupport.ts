42554627ef1c4c51ea3de487711d660a0891af00
Justin McCormick <me@justinmccormick.com>
2022-03-28 03:39:06 -0400

    fix: set standard typescript target to es5 for better interoperability

--- packages/@pdkit/nodejs/constructs/TypescriptSupport.ts
+++ packages/@pdkit/nodejs/constructs/TypescriptSupport.ts
@@ -424,7 +424,7 @@ export class TypescriptSupport extends XConstruct {
         strictNullChecks: true,
         strictPropertyInitialization: true,
         stripInternal: true,
-        target: "esnext",
+        target: "es5",
         ...props?.compilerOptions,
       },
     } as TypescriptSupportProps);
