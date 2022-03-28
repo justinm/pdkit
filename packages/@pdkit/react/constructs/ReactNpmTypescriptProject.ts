67ece1451f6bc28a44334706dc9c4b8f442515ee
Justin McCormick <me@justinmccormick.com>
2022-03-28 15:39:20 -0400

    fix: adding saner defaults for react typescript projects

--- packages/@pdkit/react/constructs/ReactNpmTypescriptProject.ts
+++ packages/@pdkit/react/constructs/ReactNpmTypescriptProject.ts
@@ -1,12 +1,16 @@
 import { XConstruct } from "@pdkit/core";
-import { NpmTypescriptProject, NpmTypescriptProjectProps } from "@pdkit/nodejs";
+import { NpmTypescriptProject, NpmTypescriptProjectProps, TypeScriptJsxMode } from "@pdkit/nodejs";
 import { ReactSupport, ReactSupportProps } from "./ReactSupport";
 
 export interface ReactNpmTypescriptProjectProps extends NpmTypescriptProjectProps, ReactSupportProps {}
 
 export class ReactNpmTypescriptProject extends NpmTypescriptProject {
   constructor(scope: XConstruct, id: string, props: ReactNpmTypescriptProjectProps) {
-    super(scope, id, props);
+    super(scope, id, {
+      sourcePath: "src",
+      ...props,
+      tsconfig: { compilerOptions: { jsx: TypeScriptJsxMode.REACT_JSX } },
+    });
 
     new ReactSupport(this, props);
   }
--- packages/@pdkit/react/constructs/ReactYarnTypescriptProject.ts
+++ packages/@pdkit/react/constructs/ReactYarnTypescriptProject.ts
@@ -1,12 +1,16 @@
 import { XConstruct } from "@pdkit/core";
-import { YarnTypescriptProject, YarnTypescriptProjectProps } from "@pdkit/nodejs";
+import { TypeScriptJsxMode, YarnTypescriptProject, YarnTypescriptProjectProps } from "@pdkit/nodejs";
 import { ReactSupport, ReactSupportProps } from "./ReactSupport";
 
 export interface ReactYarnTypescriptProjectProps extends YarnTypescriptProjectProps, ReactSupportProps {}
 
 export class ReactYarnTypescriptProject extends YarnTypescriptProject {
   constructor(scope: XConstruct, id: string, props: ReactYarnTypescriptProjectProps) {
-    super(scope, id, props);
+    super(scope, id, {
+      sourcePath: "src",
+      ...props,
+      tsconfig: { compilerOptions: { jsx: TypeScriptJsxMode.REACT_JSX } },
+    });
 
     new ReactSupport(this, props);
   }
