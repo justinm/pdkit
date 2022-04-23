866f4433754383ca9f307d820057cca8261041f8
Justin McCormick <me@justinmccormick.com>
2022-04-23 13:46:33 -0400

    feat: aws amplify support

---
+++ packages/@pdkit/core/cloud/AmplifySupport.ts
@@ -0,0 +1,27 @@
+import { GitIgnore, XConstruct } from "../core";
+
+export class AmplifySupport extends XConstruct {
+  constructor(scope: XConstruct, id: string) {
+    super(scope, id);
+
+    new GitIgnore(this, [
+      "amplify/#current-cloud-backend",
+      "amplify/.config/local-*",
+      "amplify/logs",
+      "amplify/mock-data",
+      "amplify/backend/amplify-meta.json",
+      "amplify/backend/.temp",
+      "build/",
+      "dist/",
+      "aws-exports.js",
+      "awsconfiguration.json",
+      "amplifyconfiguration.json",
+      "amplifyconfiguration.dart",
+      "amplify-build-config.json",
+      "amplify-gradle-config.json",
+      "amplifytools.xcconfig",
+      ".secret-*",
+      "**.sample",
+    ]);
+  }
+}
--- packages/@pdkit/core/index.ts
+++ packages/@pdkit/core/index.ts
@@ -1,3 +1,4 @@
 export * from "./core";
+export * as cloud from "./cloud";
 export * as nodejs from "./nodejs";
 export * as github from "./github";
