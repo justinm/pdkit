c0995e5f3c1fb0d49b38b490fa3c309f9267bc25
Justin McCormick <justin.mccormick@blackboard.com>
2022-05-13 00:10:29 -0400

    fix: allow creation of scripts

--- packages/@pdkit/core/L1/scripts/Script.ts
+++ packages/@pdkit/core/L1/scripts/Script.ts
@@ -1,6 +1,6 @@
 import { Construct } from "constructs";
 
-export abstract class Script extends Construct {
+export class Script extends Construct {
   readonly runnable: () => Promise<void>;
 
   constructor(scope: Construct, id: string, runnable: () => Promise<void>) {
